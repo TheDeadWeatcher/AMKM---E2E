5e2392ee6a30f08a24d02592d5a609f93ba9d332
Bartosz <jasykbartosz@gmail.com>
2023-08-01 16:12:11 +0200

    footer end

---
+++ cypress/e2e/footer.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+describe('Contact - E2E for AMKM group', () => {
+  before(function () {
+    cy.fixture('pagesUrl').then(function (url) {
+      globalThis.url = url;
+    });
+  });
+
+  beforeEach(() => {
+    cy.visit(url.homeUrl);
+  });
+
+  it('Should verify all links in footer', () => {
+    cy.get('[data-css_id="c9ji437"]').should('be.visible');
+    cy.requestCheckLink('[data-css_id="c9ji437"] a');
+    cy.checkLinksUrl('[data-css_id="c9ji437"] h5 a');
+  });
+});
