7342c0c87c54ed06577731e412c46abac3e3c79e
Bartosz <jasykbartosz@gmail.com>
2023-08-01 17:02:57 +0200

    AMKM e2e automated test

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,11 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   projectId: 'zzwex2',
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
 
     baseUrl: 'https://www.amkmgroup.com/',
     defaultCommandTimeout: 1000,
