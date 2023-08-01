ebb024f176deb49dcbdaf3871c3457888902e71b
Bartosz <jasykbartosz@gmail.com>
2023-08-01 16:43:34 +0200

    cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,23 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  projectId: 'zzwex2',
   e2e: {
     setupNodeEvents(on, config) {},
 
     baseUrl: 'https://www.amkmgroup.com/',
+    defaultCommandTimeout: 1000,
+    execTimeout: 10000,
+    taskTimeout: 10000,
+    pageLoadTimeout: 10000,
     includeShadowDom: true,
     viewportHeight: 1080,
     viewportWidth: 1920,
     video: false,
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx,feature}',
     retries: {
-      runMode: 0,
-      openMode: 0,
+      runMode: 4,
+      openMode: 4,
     },
   },
 });
